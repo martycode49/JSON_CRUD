a7267c9ea3754ac82f84f61336f9c474cc2f6803
martycode49 <marty49@cegetel.net>
2022-03-07 23:32:26 +0100

    Ajoutez des fichiers projet.

---
+++ JSON_CRUD/Controllers/PersonController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JSON_CRUD.Models;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.PlatformAbstractions;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace JSON_CRUD.Controllers
+{
+    public class PersonController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            JSONReadWrite readWrite = new JSONReadWrite();
+            people = JsonConvert.DeserializeObject<List<PersonModel>>(readWrite.Read("parrainagestotal.json", "data"));
+
+            var items = people.Select(p => p.Candidat).Distinct();
+            ViewBag.CandidatItems = new SelectList(items,"CandidatItems");
+            var items2 = people.Select(p => p.Departement).Distinct();
+            ViewBag.DepartItems = new SelectList(items2, "DepartItems");
+
+            return View(people);
+        }
+
+        [HttpPost]
+        public IActionResult Index(PersonModel personModel)
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            JSONReadWrite readWrite = new JSONReadWrite();
+            people = JsonConvert.DeserializeObject<List<PersonModel>>(readWrite.Read("parrainagestotal.json", "data"));
+            var key = ViewBag.CandidatItems;
+
+            IEnumerable<PersonModel> person = people.Where(
+                p => p.Candidat == personModel.Candidat 
+                && p.Departement == personModel.Departement);
+
+            return View(person);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(string name)
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            JSONReadWrite readWrite = new JSONReadWrite();
+            people = JsonConvert.DeserializeObject<List<PersonModel>>(readWrite.Read("parrainagestotal.json", "data"));
+
+            PersonModel toRemove = (PersonModel)people.Where(x => x.Nom == name);
+            people.Remove(toRemove);
+
+            string jSONString = JsonConvert.SerializeObject(people);
+            readWrite.Write("parrainagestotal.json", "data", jSONString);
+
+            return RedirectToAction("index", "Person");
+        }
+    }
+
+    public class JSONReadWrite
+    {
+        public JSONReadWrite() { }
+
+        public string Read(string fileName, string location)
+        {
+            string root = "wwwroot";
+            var path = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                root,
+                location,
+                fileName);
+
+            string jsonResult;
+
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                jsonResult = streamReader.ReadToEnd();
+            }
+            return jsonResult;
+        }
+
+        public void Write(string fileName, string location, string jSONString)
+        {
+            string root = "wwwroot";
+            var path = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                root,
+                location,
+                fileName);
+
+            using (var streamWriter = File.CreateText(path))
+            {
+                streamWriter.Write(jSONString);
+            }
+        }
+    }
+}
---
+++ JSON_CRUD/Models/PersonModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSON_CRUD.Models
+{
+    public class PersonModel
+    {
+            public string Civilite { get; set; }
+
+            public string Nom { get; set; }
+
+            public string Prenom { get; set; }
+
+            public string Mandat { get; set; }
+
+            public string Circonscription { get; set; }
+
+            public string Departement { get; set; }
+
+            public string Candidat { get; set; }
+
+            public DateTime DatePublication { get; set; }
+    }
+}
